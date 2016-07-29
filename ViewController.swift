8d17dd2f5fa07c601761e5e5fb26bc0f2319ab6e
ADITYA <aditya.h@emtres.co.id>
2016-07-30 07:29:24 +0800

    finishing login and logout facebook
    finishing login and logout facebook

--- ViewController.swift
+++ ViewController.swift
@@ -21,10 +21,36 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         view.addSubview(loginButton)
         loginButton.center = view.center
         loginButton.delegate = self
+        
+        if let token = FBSDKAccessToken.currentAccessToken(){
+            fetchProfile()
+        }
+    }
+    func fetchProfile(){
+        print("fetch profile")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler{(connection, result, error) -> Void in
+            
+            if error != nil{
+                print(error)
+                return
+            }
+            
+            if let email = result["email"] as? String{
+                print(email)
+            }
+            
+            if let picture = result["picture"] as? NSDictionary, data = picture["data"] as? NSDictionary, url = data["url"] as? String{
+                print(url)
+            }
+        }
+
     }
 
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
         print ("completed login")
+        fetchProfile()
     }
     
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
