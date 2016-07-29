4cad67099afe06a1d6f6ba7033e7bd134f89fc74
ADITYA <aditya.h@emtres.co.id>
2016-07-29 08:35:45 +0800

    app init
    app init

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookLogin
+//
+//  Created by aditya on 7/29/16.
+//  Copyright © 2016 aditya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
